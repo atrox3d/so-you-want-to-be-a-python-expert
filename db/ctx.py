c033effa159a9ee415777dd53e6d41f1f18c8be0
atrox3d <atrox3d@gmail.com>
2023-12-27 13:27:03 +0100

    add TempTable class

--- db/ctx.py
+++ db/ctx.py
@@ -1,3 +1,7 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 # with ctx() as x:
 #     pass
 
@@ -7,3 +11,19 @@
 # finally:
 #     x.__exit__()
 
+class TempTable:
+    def __init__(self, cur, createsql, dropsql):
+        logger.info(f'{cur = }')
+        logger.info(f'{createsql = }')
+        logger.info(f'{dropsql = }')
+        self.cur = cur
+        self.create = createsql
+        self.drop = dropsql
+
+    def __enter__(self):
+        logger.info(self.create)
+        self.cur.execute(self.create)
+
+    def __exit__(self, *args):
+        logger.info(self.drop)
+        self.cur.execute(self.drop)
--- db/db.py
+++ db/db.py
@@ -1,28 +1,34 @@
 from sqlite3 import connect
 import logging
 
+from .ctx import TempTable
+
 logger = logging.getLogger(__name__)
 
 
 def demo():
     with connect('test.db') as conn:
         cur = conn.cursor()
-        logger.info('create table points (x int, y, int)')
-        cur.execute('create table points (x int, y, int)')
-        
-        logger.info('insert into points (x, y) values(*, *)')
-        cur.execute('insert into points (x, y) values(1, 2)')
-        cur.execute('insert into points (x, y) values(2, 3)')
-        cur.execute('insert into points (x, y) values(4, 5)')
+        with TempTable( cur, 
+                        'create table points (x int, y, int)', 
+                        'drop table points'
+                        ):
+            # logger.info('create table points (x int, y, int)')
+            # cur.execute('create table points (x int, y, int)')
+            
+            logger.info('insert into points (x, y) values(*, *)')
+            cur.execute('insert into points (x, y) values(1, 2)')
+            cur.execute('insert into points (x, y) values(2, 3)')
+            cur.execute('insert into points (x, y) values(4, 5)')
 
-        logger.info('selects')
-        for row in cur.execute('select x, y from points'):
-            logger.info(f'{row = }')
+            logger.info('selects')
+            for row in cur.execute('select x, y from points'):
+                logger.info(f'{row = }')
 
-        logger.info('sum')
-        for row in cur.execute('select sum(x*y) from points'):
-            logger.info(f'{row = }')
-        
-        logger.info('drop table points')
-        cur.execute('drop table points')
-        
+            logger.info('sum')
+            for row in cur.execute('select sum(x*y) from points'):
+                logger.info(f'{row = }')
+            
+            # logger.info('drop table points')
+            # cur.execute('drop table points')
+            
