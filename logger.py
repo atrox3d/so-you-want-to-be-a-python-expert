5487ef257c5450ce4dabcb33591bc9d4100e659e
atrox3d <atrox3d@gmail.com>
2023-12-27 09:02:50 +0100

    adding logger

---
+++ logger.py
@@ -0,0 +1,37 @@
+import logging
+import sys
+
+
+def get_logger(
+        name, 
+        level=logging.INFO, 
+        modulename_width=12, 
+        funcname_width=12, 
+        levelname_width=len("CRITICAL"),
+        line_format=None,
+        date_format=None
+        ):
+    """
+    get standard logger to stdout, level INFO
+    :param name:
+    :param level:
+    :return:
+    """
+
+    line_format = line_format or (
+            f"%(asctime)s | "
+            f"%(module){modulename_width}s.py | "
+            f"%(funcName){funcname_width}s() | "
+            f"%(levelname)-{levelname_width}s | "
+            f"%(message)s"
+    )
+    date_format = date_format or '%Y/%m/%d %H:%M:%S'
+
+    logging.basicConfig(
+        level=level,
+        format=line_format,
+        datefmt=date_format,
+        # stream=sys.stdout
+    )
+    logger = logging.getLogger(name)
+    return logger
---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+import argparse
+import logging
+
+from logger import get_logger
+
+logger = get_logger(__name__, level=logging.DEBUG)
+
+def get_args():
+    args = sys.argv[1:]
+    logger.debug(f'returning {args = }, {{}}')
+    return args, {}
+
+def parse_args():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('args', nargs='*')
+        # parser.add_argument('-f', )
+        args = parser.parse_args()
+        print(args.args)
+
+def main(*args, **kwargs):
+    logger.debug(f'{args = }')
+    logger.debug(f'{kwargs = }')
+    return 0
+
+if  __name__ == '__main__':
+
+    logging.basicConfig(level=logging.DEBUG)
+    args, kwargs = get_args()
+    logger.debug(f'{args = }')
+    logger.debug(f'{kwargs = }')
+
+    sys.exit(main(*args, **kwargs))
