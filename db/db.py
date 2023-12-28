a67c9e9eb1f4c9f65ca94503fa2121fb3d608999
atrox3d <atrox3d@gmail.com>
2023-12-28 05:32:01 +0100

    with conn

--- db/db.py
+++ db/db.py
@@ -1,4 +1,5 @@
 from sqlite3 import connect
+from contextlib import closing
 import logging
 
 from .ctx import TempTable
@@ -7,24 +8,26 @@ logger = logging.getLogger(__name__)
 
 
 def demo():
-    with connect('test.db') as conn:
-        cur = conn.cursor()
-        createsql='CREATE TABLE points (x int, y, int)'
-        dropsql='DROP TABLE points'
+    # closes the connection
+    with closing(connect('test.db')) as conn:
+        # commits changes
+        with conn:
+            cur = conn.cursor()
+            createsql='CREATE TABLE points (x int, y, int)'
+            dropsql='DROP TABLE points'
+            # creates and drops the table
+            with TempTable( cur, createsql, dropsql):
+                logger.info('INSERT INTO points (x, y) VALUES(*, *)')
+                cur.execute('INSERT INTO points (x, y) VALUES(1, 2)')
+                cur.execute('INSERT INTO points (x, y) VALUES(2, 3)')
+                cur.execute('INSERT INTO points (x, y) VALUES(4, 5)')
 
-        with TempTable( cur, createsql, dropsql):
-            logger.info('INSERT INTO points (x, y) VALUES(*, *)')
-            cur.execute('INSERT INTO points (x, y) VALUES(1, 2)')
-            cur.execute('INSERT INTO points (x, y) VALUES(2, 3)')
-            cur.execute('INSERT INTO points (x, y) VALUES(4, 5)')
+                sql = 'SELECT x, y FROM points'
+                logger.info(sql)
+                for row in cur.execute(sql):
+                    logger.info(f'{row = }')
 
-            sql = 'SELECT x, y FROM points'
-            logger.info(sql)
-            for row in cur.execute(sql):
-                logger.info(f'{row = }')
-
-            sql = 'SELECT sum(x*y) FROM points'
-            logger.info(sql)
-            for row in cur.execute(sql):
-                logger.info(f'{row = }')
-            
+                sql = 'SELECT sum(x*y) FROM points'
+                logger.info(sql)
+                for row in cur.execute(sql):
+                    logger.info(f'{row = }')
