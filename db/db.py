c15eb642a12cc8a4850e9642587952f750a8bd2f
atrox3d <atrox3d@gmail.com>
2023-12-28 04:13:52 +0100

    query code optimization

--- db/db.py
+++ db/db.py
@@ -9,26 +9,22 @@ logger = logging.getLogger(__name__)
 def demo():
     with connect('test.db') as conn:
         cur = conn.cursor()
-        with TempTable( cur, 
-                        'create table points (x int, y, int)', 
-                        'drop table points'
-                        ):
-            # logger.info('create table points (x int, y, int)')
-            # cur.execute('create table points (x int, y, int)')
-            
-            logger.info('insert into points (x, y) values(*, *)')
-            cur.execute('insert into points (x, y) values(1, 2)')
-            cur.execute('insert into points (x, y) values(2, 3)')
-            cur.execute('insert into points (x, y) values(4, 5)')
+        createsql='CREATE TABLE points (x int, y, int)', 
+        dropsql='DROP TABLE points'
 
-            logger.info('selects')
-            for row in cur.execute('select x, y from points'):
+        with TempTable( cur, createsql, dropsql):
+            logger.info('INSERT INTO points (x, y) VALUES(*, *)')
+            cur.execute('INSERT INTO points (x, y) VALUES(1, 2)')
+            cur.execute('INSERT INTO points (x, y) VALUES(2, 3)')
+            cur.execute('INSERT INTO points (x, y) VALUES(4, 5)')
+
+            sql = 'SELECT x, y FROM points'
+            logger.info(sql)
+            for row in cur.execute(sql):
                 logger.info(f'{row = }')
 
-            logger.info('sum')
-            for row in cur.execute('select sum(x*y) from points'):
+            sql = 'SELECT sum(x*y) FROM points'
+            logger.info(sql)
+            for row in cur.execute(sql):
                 logger.info(f'{row = }')
-            
-            # logger.info('drop table points')
-            # cur.execute('drop table points')
             
